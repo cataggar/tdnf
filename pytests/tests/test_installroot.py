cataggar/tdnf

commit e8906072bd545e5a0a4cac72e1a5de80bb9b155c

changes for running on Ubuntu/Debian

client/api.c
@@ -280,7 +280,9 @@ TDNFCheckLocalPackages(
 
     pr_info("Checking all packages from: %s\n", pszLocalPath);
 
-    pCmdLinePool = pool_create();
+    dwError = SolvCreatePool(&pCmdLinePool);
+    BAIL_ON_TDNF_ERROR(dwError);
+
     pool_set_rootdir(pCmdLinePool, pTdnf->pArgs->pszInstallRoot);
 
     pCmdlineRepo = repo_create(pCmdLinePool, CMDLINE_REPO_NAME);
@@ -715,6 +717,9 @@ TDNFOpenHandle(
     dwError = TDNFInitCmdLineRepo(pTdnf, pSack);
     BAIL_ON_TDNF_ERROR(dwError);
 
+    pool_addfileprovides(pSack->pPool);
+    pool_createwhatprovides(pSack->pPool);
+
     pTdnf->pSack = pSack;
     *ppTdnf = pTdnf;
 
@@ -853,6 +858,8 @@ TDNFAddCmdLinePackages(
         }
         queue_push(pQueueGoal, id);
     }
+
+    pool_addfileprovides(pSack->pPool);
     pool_createwhatprovides(pSack->pPool);
     repo_internalize(pTdnf->pSolvCmdLineRepo);
 

client/repo.c
@@ -99,6 +99,7 @@ TDNFInitRepo(
         BAIL_ON_TDNF_ERROR(dwError);
     }
 
+    pool_addfileprovides(pPool);
     pool_createwhatprovides(pPool);
 
 cleanup:
@@ -194,8 +195,6 @@ TDNFInitCmdLineRepo(
     pRepo->appdata = pSolvRepoInfo;
 
     pTdnf->pSolvCmdLineRepo = pRepo;
-
-    pool_createwhatprovides(pPool);
 
 cleanup:
     TDNF_SAFE_FREE_MEMORY(pSolvRepoInfo);
@@ -972,7 +971,9 @@ TDNFParseRepoMD(
         BAIL_ON_TDNF_ERROR(dwError);
     }
 
-    pPool = pool_create();
+    dwError = SolvCreatePool(&pPool);
+    BAIL_ON_TDNF_ERROR(dwError);
+
     pRepo = repo_create(pPool, "md_parse_temp");
 
     fp = fopen(pRepoMD->pszRepoMD, "r");
@@ -1147,7 +1148,9 @@ TDNFDownloadMetadata(
         pr_info("%s\n", pszRepoMDUrl);
     }
 
-    pPool = pool_create();
+    dwError = SolvCreatePool(&pPool);
+    BAIL_ON_TDNF_ERROR(dwError);
+
     pSolvRepo = repo_create(pPool, "md_parse_temp");
 
     fp = fopen(pszRepoMDPath, "r");

solv/prototypes.h
@@ -341,6 +341,9 @@ SolvFreeSack(
     PSolvSack);
 
 uint32_t
+SolvCreatePool(Pool **ppPool);
+
+uint32_t
 SolvInitSack(
     PSolvSack *ppSack,
     const char* pszCacheDir,

solv/tdnfpool.c
@@ -64,6 +64,44 @@ SolvFreeSack(
 }
 
 uint32_t
+SolvCreatePool(Pool **ppPool)
+{
+    uint32_t dwError = 0;
+    Pool* pPool = NULL;
+
+    pPool = pool_create();
+
+    if(!ppPool)
+    {
+        dwError = ERROR_TDNF_INVALID_PARAMETER;
+        BAIL_ON_TDNF_LIBSOLV_ERROR(dwError);
+    }
+
+    if(pPool == NULL)
+    {
+        dwError = ERROR_TDNF_INVALID_PARAMETER;
+        BAIL_ON_TDNF_LIBSOLV_ERROR(dwError);
+    }
+
+    if (pool_setdisttype(pPool, DISTTYPE_RPM) < 0) {
+        dwError = ERROR_TDNF_INVALID_PARAMETER;
+        BAIL_ON_TDNF_LIBSOLV_ERROR(dwError);
+    }
+
+    *ppPool = pPool;
+
+cleanup:
+    return dwError;
+
+error:
+    if(pPool)
+    {
+        pool_free(pPool);
+    }
+    goto cleanup;
+}
+
+uint32_t
 SolvInitSack(
     PSolvSack *ppSack,
     const char* pszCacheDir,
@@ -91,12 +129,8 @@ SolvInitSack(
         BAIL_ON_TDNF_ERROR(dwError);
     }
 
-    pPool = pool_create();
-    if(pPool == NULL)
-    {
-        dwError = ERROR_TDNF_INVALID_PARAMETER;
-        BAIL_ON_TDNF_LIBSOLV_ERROR(dwError);
-    }
+    dwError = SolvCreatePool(&pPool);
+    BAIL_ON_TDNF_LIBSOLV_ERROR(dwError);
 
     if(pszRootDir != NULL)
     {

commit 3313ab34c7b795ab9fd9bca8ebe61a5e643a3865

Fix package installs from command line repo
When installing packages from the command line, repo_internalize() must
be called first, before pool_addfileprovides() and pool_createwhatprovides()

Fixes: e8906072

Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>

client/api.c
@@ -832,9 +832,9 @@ TDNFAddCmdLinePackages(
         queue_push(pQueueGoal, id);
     }
 
+    repo_internalize(pTdnf->pSolvCmdLineRepo);
     pool_addfileprovides(pSack->pPool);
     pool_createwhatprovides(pSack->pPool);
-    repo_internalize(pTdnf->pSolvCmdLineRepo);
 
 cleanup:
     TDNF_SAFE_FREE_MEMORY(pszRPMPath);

pytests/repo/tdnf-test3.spec
@@ -1,0 +1,24 @@
+Summary:    basic install test file.
+Name:       tdnf-test3
+Version:    1.0
+Release:    1
+Vendor:     VMware, Inc.
+License:    VMware
+Url:        http://www.vmware.com
+Group:      Applications/tdnftest
+Distribution:   Photon
+
+%description
+Spec to test install rpms from local repo
+
+%prep
+%build
+%install
+
+mkdir -p %{buildroot}%{_bindir}
+echo "one" > %{buildroot}%{_bindir}/one
+
+%files
+%{_bindir}/one
+
+%changelog

pytests/repo/tdnf-test4.spec
@@ -1,0 +1,22 @@
+Summary:    basic install test file.
+Name:       tdnf-test4
+Version:    1.0
+Release:    1
+Vendor:     VMware, Inc.
+License:    VMware
+Url:        http://www.vmware.com
+Group:      Applications/tdnftest
+Distribution:   Photon
+
+Requires(post): /usr/bin/one
+
+%description
+Spec to test install rpms from local repo
+
+%prep
+%build
+%install
+
+%files
+
+%changelog

pytests/tests/test_installroot.py
@@ -7,12 +7,15 @@
 #
 
 import os
+import glob
 import shutil
 import pytest
 import fnmatch
+import tempfile
 
 INSTALLROOT = '/root/installroot'
 REPOFILENAME = 'photon-test.repo'
+RELEASE_VER = '5.0'
 
 REPODIR = '/root/yum.repos.d'
 REPONAME = 'reposdir-test'
@@ -128,3 +131,42 @@ def test_setopt_reposdir_with_installroot(utils):
     assert REPONAME in "\n".join(ret['stdout'])
 
     shutil.rmtree(INSTALLROOT)
+
+
+def test_installroot_local_rpms_disablerepo_after_download(utils):
+
+    workdir = tempfile.mkdtemp(prefix='test-tdnf-')
+    try:
+        installroot = os.path.join(workdir, 'installroot')
+        rpm_repo = os.path.join(workdir, 'rpm-repo')
+        os.makedirs(rpm_repo, exist_ok=True)
+        os.makedirs(installroot, exist_ok=True)
+
+        repo_url = "http://localhost:8080/photon-test"
+
+        ret = utils.run(
+            ['tdnf', 'install', '-y', 'tdnf-test3', 'tdnf-test4',
+             '--refresh', '--downloadonly',
+             '--releasever', RELEASE_VER,
+             '--installroot', installroot,
+             '--downloaddir', rpm_repo,
+             '--nogpgcheck',
+             '--disablerepo=*', '--enablerepo=pkgs',
+             f'--repofrompath=pkgs,{repo_url}'],
+            cwd=workdir
+        )
+        assert ret['retval'] == 0, 'downloadonly failed: {}'.format(ret.get('stderr', []))
+
+        rpms = glob.glob(os.path.join(rpm_repo, '*.rpm'))
+        assert len(rpms) >= 1, 'no RPMs downloaded into rpm-repo'
+
+        ret = utils.run(
+            ['tdnf', 'install', '-y', '--refresh', '--releasever', RELEASE_VER,
+             '--installroot', installroot, '--disablerepo=*', '--nogpgcheck'] + rpms,
+            cwd=workdir
+        )
+        assert ret['retval'] == 0, (
+            'install from local RPMs into installroot with --disablerepo=* failed: {}'
+        ).format(ret.get('stderr', []))
+    finally:
+        shutil.rmtree(workdir, ignore_errors=True)